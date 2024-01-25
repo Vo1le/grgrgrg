6255a4da1b7b9020417cb762f8fdeb790d3770a7
Volte <82891603+Vo1le@users.noreply.github.com>
2024-01-25 13:28:20 +0100

    Update temp.py

--- algo/temp.py
+++ algo/temp.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 #diabetic=pandas.read_csv("diabetes_data.csv")
 diabetic13=pandas.read_csv("diabetes_data13.csv")
 diabetic23=pandas.read_csv("diabetes_data23.csv")
-
-para1 = "age"
+s=0
+para1 = "bmi"
 para2 = "glucose"
 para3 = "insulin"
 
@@ -22,38 +22,36 @@ ax.scatter(x[lab == 0], y[lab == 0],z[lab == 0], color='r', label='négatif')
 def distance (xA,yA,zA,xB,yB,zB): 
     return ((xB-xA)**2+(yB-yA)**2+(zB-zA)**2)**0.5
 
-def knn (dataframe,k,xA,yA,zA):
+
+result = 0
+
+def knn (dataframe,k,xA,yA,zA,t):
     dataframe["distance"]= distance(xA,yA,zA,dataframe[para1],dataframe[para2],dataframe[para3])
     dataframe.sort_values(by=['distance'], inplace=True)
-    resultat1 = 0
-    resultat2 = 0
+    resultat = 0
+    global result
     for i in range(0,k,1):
-       if dataframe.loc[i,"diabetes"] == 1:
-           resultat1 = resultat1 + 1
-       else:
-           resultat2 = resultat2 + 1
-           
-    if resultat1 > resultat2 :
-        return 1
-    elif resultat1 < resultat2:
-        return 0
-    else: 
-        return int(0)
+       if dataframe.iloc[i,8] == 1:
+           resultat = 1
+       elif dataframe.iloc[i,8] == 0:
+           resultat = 0
+    if resultat == diabetic13.loc[t,"diabetes"]:
+            result = result + 1
 
 def test(k):
-    s=0
-    result =0
+    global result
+    result = 0
     best = 0
     
     for t in range(0,len(diabetic13)):
         a = diabetic13.loc[t,para1]
         b = diabetic13.loc[t,para2]
         c = diabetic13.loc[t,para3]
-        print(a,b,c)
-        s = knn(diabetic23,k,a,b,c)
-        print(s, diabetic13.loc[t,"diabetes"])
-        if s == diabetic13.loc[t,"diabetes"]:
-            result = result + 1
+
+        knn(diabetic23,k,a,b,c,t)
+
     best = (result / 258)*100
     print(best , " " , k)
     return best
+for p in range(0,10,1):
+    test(p)
